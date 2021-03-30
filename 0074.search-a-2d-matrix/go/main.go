3964a652cfc36b953f9d41513aaec485e1378ba8
hustlibraco <hustlibraco@gmail.com>
2021-03-30 16:15:05 +0800

    update 0074. go solution

--- 0074.search-a-2d-matrix/go/main.go
+++ 0074.search-a-2d-matrix/go/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+/*
+编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+
+每行中的整数从左到右按升序排列。
+每行的第一个整数大于前一行的最后一个整数。
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/search-a-2d-matrix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
 
 func searchMatrix(matrix [][]int, target int) bool {
+	return searchMatrix2(matrix, target)
+}
+
+func searchMatrix1(matrix [][]int, target int) bool {
+	/*
+		先找行再找列
+	*/
 	rows, cols := len(matrix), len(matrix[0])
 	if rows == 0 || cols == 0 || matrix[0][0] > target || matrix[rows-1][cols-1] < target {
 		return false
@@ -29,6 +50,33 @@ func searchMatrix(matrix [][]int, target int) bool {
 			i = mid + 1
 		} else {
 			return true
+		}
+	}
+	return false
+}
+
+func searchMatrix2(matrix [][]int, target int) bool {
+	/*
+		由于矩阵整个是有序的，所以可以当作一个有序数组，只用一次二分查找
+	*/
+	m := len(matrix)
+
+	if m == 0 {
+		return false
+	}
+	n := len(matrix[0])
+
+	l := 0
+	r := m*n - 1
+
+	for l <= r {
+		mid := (l + r) / 2
+		if matrix[mid/n][mid%n] == target {
+			return true
+		} else if matrix[mid/n][mid%n] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
 		}
 	}
 	return false
